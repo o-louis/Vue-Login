259e08bfab9d325a8d373e6d0a1592bcf2616635
o-louis <oriane.louis.pro@gmail.com>
2021-03-18 12:33:52 -0400

    chore(path): Import path instead of require it

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import vue from "@vitejs/plugin-vue";
+import path from "path";
 import { defineConfig } from "vite";
-const path = require("path");
 
-// https://vitejs.dev/config/
 export default defineConfig({
   alias: { "@": path.resolve(__dirname, "./src") },
   plugins: [vue()],
