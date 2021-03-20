0c0318fbd87a52d93b0d1cd4b5761b982d62d9f5
o-louis <oriane.louis.pro@gmail.com>
2021-03-19 21:03:23 -0400

    feat(TheHeader): Create Header component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
 <template>
+  <Header title="VueLogin" home="/" />
   <router-view />
 </template>
+
+<script>
+import Header from "@/components/TheHeader.vue";
+export default {
+  components: { Header },
+};
+</script>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <header
+    class="w-full max-w-screen-lg py-5 m-auto text-3xl font-bold text-white"
+  >
+    <router-link :to="home">{{ title }}</router-link>
+  </header>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    home: String,
+  },
+};
+</script>
