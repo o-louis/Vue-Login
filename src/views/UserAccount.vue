185223711f5f957ae1463df0f71facad27af8cde
o-louis <oriane.louis.pro@gmail.com>
2021-03-19 23:46:25 -0400

    feat(UserAccount): Add content and display name account

--- src/views/UserAccount.vue
+++ src/views/UserAccount.vue
@@ -1,11 +1,21 @@
 <template>
-  <div>Account</div>
+  <main
+    class="w-full max-w-screen-lg m-auto mt-10 text-lg leading-relaxed text-white"
+  >
+    <h1 class="mt-10 mb-4 text-2xl font-bold">Hello {{ name }} 🤙</h1>
+    <p>You are now connected ! Congrats !</p>
+  </main>
 </template>
 
 <script>
+import { useStore } from "vuex";
+
 export default {
   setup() {
-    return {};
+    const store = useStore();
+    return {
+      name: store.state.user.name,
+    };
   },
 };
 </script>
