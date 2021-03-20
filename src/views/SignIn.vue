f4c07ccd68d1649ca6be972f43350ecb125bff30
o-louis <oriane.louis.pro@gmail.com>
2021-03-19 23:24:46 -0400

    feat(SignIn): Add content + redirect to / after submit form

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,11 +1,33 @@
 <template>
-  <div>SignIn</div>
+  <main
+    class="w-full max-w-screen-lg m-auto mt-10 text-lg leading-relaxed text-white"
+  >
+    <h1 class="mb-4 text-2xl font-bold uppercase text-pink">
+      Sign In to get a surprise
+    </h1>
+    <FormSignIn @onSubmit="redirectToAccount" />
+  </main>
 </template>
 
 <script>
+import FormSignIn from "@/components/FormSignIn.vue";
+import router from "@/router";
+
+import { useStore } from "vuex";
+
 export default {
+  components: { FormSignIn },
   setup() {
-    return {};
+    const store = useStore();
+
+    const redirectToAccount = () => {
+      store.dispatch("user/authentication");
+      router.push({ name: "Home" });
+    };
+
+    return {
+      redirectToAccount,
+    };
   },
 };
 </script>
