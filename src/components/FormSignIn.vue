6e2f1be023307b8e971b60ef5e0b992f0287e885
o-louis <oriane.louis.pro@gmail.com>
2021-03-19 23:22:17 -0400

    feat(FormSignIn): Create the form for sign in

---
+++ src/components/FormSignIn.vue
@@ -0,0 +1,71 @@
+<template>
+  <form
+    @submit.prevent="onSubmit()"
+    class="max-w-xs p-4 text-black bg-white rounded-md"
+  >
+    <label for="username" class="flex flex-col"
+      >Username
+      <input
+        type="text"
+        name="username"
+        placeholder="Enter your username..."
+        class="pl-3 mt-1 border border-gray-300"
+        v-model="username"
+      />
+    </label>
+
+    <label for="password" class="flex flex-col mt-6"
+      >Password
+      <input
+        type="password"
+        name="password"
+        placeholder="Enter your password..."
+        class="pl-3 mt-1 border border-gray-300"
+        v-model="password"
+      />
+    </label>
+    <p v-if="error">Please fill all field</p>
+    <button
+      type="submit"
+      class="w-full py-1 mt-8 text-lg font-semibold text-white uppercase rounded-md bg-pink hover:opacity-90"
+    >
+      Sign in
+    </button>
+  </form>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  name: "FormSignIn",
+  props: {
+    error: Boolean,
+  },
+  setup(props, { emit }) {
+    const username = ref("");
+    const password = ref("");
+    const error = ref(false);
+
+    const onSubmit = () => {
+      error.value = false;
+      if (username.value && password.value) {
+        emit("onSubmit", {
+          username: username.value,
+          password: password.value,
+        });
+        username.value = "";
+        password.value = "";
+      } else {
+        error.value = true;
+      }
+    };
+    return {
+      error,
+      username,
+      password,
+      onSubmit,
+    };
+  },
+};
+</script>
